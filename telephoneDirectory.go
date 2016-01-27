244ffe3f95f7e8f1c660ca0c009d73eaea8e6b81
Cam <dyluth@gmail.com>
2016-01-27 00:00:40 +0000

    got a working simple prototype encoding post data and then decoding a value out.

--- telephoneDirectory.go
+++ telephoneDirectory.go
@@ -17,6 +17,7 @@ func StartServer(c chan int ) {
 
 	//start the webserver listening on port 8084
 	//redirect to DirectoryServer
+	//have a channel to keep the main process open until the server fails somehow
 	http.HandleFunc("/directory", DirectoryServer)
 	err := http.ListenAndServe(":8084", nil)
 	if err != nil {
@@ -27,4 +28,17 @@ func StartServer(c chan int ) {
 
 func DirectoryServer(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "ooh! Questions!\n")
+	//io.WriteString(w, req.Method) //eg "GET"
+	//look into the request to see what the query values are
+	req.ParseForm()
+	//io.WriteString(w, "form: "+req.Form.Encode())
+		fmt.Print("request ")
+		fmt.Println(req)
+		fmt.Print("form ")
+		fmt.Println(req.PostForm)
+		fmt.Println(req.PostFormValue("cake"))
+	//io.WriteString(w, req)
+
+	//in the body, in the vars somewhere..
+
 }
--- telephoneDirectory_test.go
+++ telephoneDirectory_test.go
@@ -5,14 +5,59 @@ import (
 	"net/http"
 	"testing"
 	"io/ioutil"
+	"net/url"
+	"strings"
+	
+    //"bytes"
 )
 
-
-
-func TestServer(t *testing.T) {
+func TestMain(t *testing.T) {
 	c := make(chan int)
 	go StartServer(c)
+
+}
+
+func TestServerGet(t *testing.T) {
+	t.Log("testing the server")
+	
+	form := url.Values{}
+    form.Set("user", "dave")
+	form.Add("cake", "yes please")
+	
+	//req,err := http.NewRequest("GET", "http://localhost:8084/directory", bytes.NewBufferString(form.Encode()))
+	//t.Log("request to send:  ")
+	//t.Log(req)
+	//if err != nil {
+	//	t.Fatal(err)
+	//}
+	//response, err := http.DefaultClient.Do(req)
+
+	
+	response, err := http.DefaultClient.PostForm("http://localhost:8084/directory", form)
+	 
+	if err != nil {
+		t.Fatal(err)
+		
+	}
+    t.Log("status: ",response.Status)
+	
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	s := string(contents[:])
+	t.Log("contents: ",  s)
+	
+	//t.Log("time to fail?")
+	t.FailNow()
+	
+}
+
+func TestServer(t *testing.T) {
+	//c := make(chan int)
+	//go StartServer(c)
 	fmt.Println("testing the server")
+		
 	response, err := http.Get("http://localhost:8084/directory")
 	fmt.Println("we got: ", response, "and error: ", err)
 	if err != nil {
@@ -20,14 +65,14 @@ func TestServer(t *testing.T) {
 	}
 	contents, err := ioutil.ReadAll(response.Body)
 	
-	fmt.Println("contents: ", contents, "and err: ", err)
 	if err != nil {
 		t.Fatal(err)
 	}
 	s := string(contents[:])
 	fmt.Println("contents: ",  s)
 	
-	if(s != "ooh! Questions!\n") {
+	if(!strings.HasPrefix(s, "ooh! Questions!")) {
+		fmt.Println("did not get the expected response")
 		t.FailNow()
 	}	
 }
